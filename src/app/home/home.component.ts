d9f58478b4ef7826edcf492446cbc36c6adc305c
GrilloDavide <grillo.davide@itismeucci.com>
2024-05-02 21:59:02 +0200

    aggiunto form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatButtonToggleModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  
+   isAcceptable() {
+    
+  }
 
+  
+  fontStyleControl = new FormControl('');
+  fontStyle?: string;
 }
