fc107bcd9e012b1ee1faaeaca7edf7e4c3765f22
GrilloDavide <grillo.davide@itismeucci.com>
2024-05-04 22:24:13 +0200

    sviluppo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -7,17 +7,29 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatButtonToggleModule, ReactiveFormsModule, MatInputModule],
+  imports: [MatButtonModule, MatButtonToggleModule, ReactiveFormsModule, MatInputModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   
-   isAcceptable() {
-    
-  }
+  proprieties = this.formBuilder.group({
+    choice: [''],
+    inputFunction: ['']
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+  
+  isAcceptable(){
+    console.log(this.proprieties.value)
+  }
   
   fontStyleControl = new FormControl('');
   fontStyle?: string;
+
+
+
+  onSubmit(){
+
+  }
 }
